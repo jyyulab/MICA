f1272516c608efc1424c65a1732f9229fa977f04
Tracy Qian <chenxi.qian@stjude.org>
2019-06-24 16:23:15 -0500

    first commit for cwl

--- MICA/hclust.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-import utils
-
-utils.hclust(int(sys.argv[1]), sys.argv[2], sys.argv[3], sys.argv[4])
--- setup.py
+++ setup.py
@@ -7,28 +7,21 @@ with open("./version.py") as fp:
 setup(
     name="MICA",
     version=version["__version__"],
-    description="Mutual Information-based Clustering Analysis",
+    description="Mutual Information based Clustering analysis",
     url="https://github.com/jyyulab/MICA",
-    author="Liang Ding, Chenxi Qian, Alireza Khatamian",
-    author_email="liang.ding@stjude.org",
-    license="Apache License 2.0",
+    author="Chenxi Qian, Liang Ding, Alireza Khatamian",
+    author_email="chenxi.qian@stjude.org, liang.ding@stjude.org",
+    license="See LICENSE.md",
     install_requires=[
-        "pandas >= 0.22.0",
-        "numpy >= 1.14.2",
-        "scikit-learn >= 0.19.1",
-        "matplotlib >= 2.2.2",
-        "scipy >= 1.0.1",
+        "pandas>=0.22.0",
+        "numpy>=1.14.2",
+        "scikit-learn>=0.19.1",
+        "matplotlib>=2.2.2",
+        "scipy>=1.0.1",
     ],
     python_requires="==3.6.1",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    package_data={
-        "MICA": ["ggplot.cc.r"],
-    },
     include_package_data=True,
     test_suite="tests",
-    entry_points={
-        "console_scripts": [
-            "mica=MICA.mica:main",
-        ]
-    },
+    entry_points={"console_scripts": ["mica=MICA.MICA:main"]},
 )
