4b452bb78bad451680191635cd9fa04a08bde0d2
Jimmy Veloso <joaopvapv100@yahoo.com.br>
2019-07-23 12:32:25 -0500

    Added acceptance testing, increased speed of MI calc, added argparse to scripts, added travis-ci support

--- MICA/bin/03_merge_and_norm.py
+++ MICA/bin/03_merge_and_norm.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 import utils
 
-project_name = sys.argv[1]
-method = sys.argv[2]
-mat_files = sys.argv[3:]
+def main():
+    """Handles arguments and calls the driver function"""
+    head_description = 'Merges a set of matrices into one' 
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=head_description)
+    parser.add_argument('-i', '--mi-slices', nargs='+', metavar='STR', required=True, help='List of matrices')
+    parser.add_argument('-o', '--proj-name', metavar='STR', required=True, help='Project name used for previous steps')
+    parser.add_argument('-m', '--metric', metavar='STR', required=True, help='Metric used in calculation')
+    args=parser.parse_args()
 
-utils.merge_dist_mats(mat_files, project_name, method)
+    merge_and_norm(args.mi_slices, args.proj_name, args.metric.lower())
 
-dist_file = project_name + "_dist.h5"
 
-if method == "mi":
-    utils.norm_mi_mat(dist_file, project_name)
+def merge_and_norm(mat_files, project_name, method):
+    """Merges matrices, and normalizes numeric data when distance metric is mutual information.
+
+    Calls on utility functions to merge distance metrics that were calculated in between slices.
+    Normalizes the resulting merged matrix when the data that was originally calculated was a
+    measure of mutual information.
+
+    Args:
+        mat_files  (str[]): list of sliced files comparing slices
+        project_name (str): name of project/original output name
+        method       (str): metric used for calculating distance
+    """
+
+    utils.merge_dist_mats(mat_files, project_name, method)
+    dist_file = project_name + "_dist.h5"
+    if method == "mi":
+        utils.norm_mi_mat(dist_file, project_name)
+
+
+if __name__ == '__main__':
+    main()
