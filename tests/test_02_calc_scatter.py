77991bb69e855c192fcc420046d26a1292537f2c
Jimmy Veloso <joaopvapv100@yahoo.com.br>
2019-07-25 11:54:18 -0500

    Adding almost_equal assertion to calc_scatter testing

--- tests/test_02_calc_scatter.py
+++ tests/test_02_calc_scatter.py
@@ -2,6 +2,7 @@
 
 import unittest
 import tempfile
+import numpy as np
 import pandas as pd
 from MICA.bin import calc_scatter as cs
 
@@ -21,7 +22,16 @@ class TestCalcScatter(unittest.TestCase):
         cs.calc_scatter(self.infile, "mi")
         pd_test = pd.HDFStore(self.folder.name + "/test_mi_04.h5")
         pd_ans = pd.HDFStore("./tests/answerkey/calc_scatter/test_mi_04.h5")
-        self.assertTrue(pd_test['mi_04'].equals(pd_ans['mi_04']))    
+        mi_test = pd_test['mi_04']
+        mi_ans = pd_ans['mi_04']
+        almostEq = True
+        for i in  mi_test.index:
+            arr_test = mi_test.loc[i]
+            arr_ans = mi_ans.loc[i]
+            for i in range(0, 100):
+                np.testing.assert_almost_equal(arr_test[i], arr_ans[i])
+
+        self.assertTrue(almostEq)
 
 
 if __name__ == '__main__':
