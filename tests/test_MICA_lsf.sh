a84d92bad90af174b10a47a68df48c6841b81a81
Liang Ding <adamdingliang@gmail.com>
2019-09-06 15:35:54 -0500

    another round of final refinement

--- tests/test_MICA_lsf.sh
+++ tests/test_MICA_lsf.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 mica lsf \
-<<<<<<< HEAD
--i /research/projects/yu3grp/scRNASeq/yu3grp/TracyQian/scMINER/dev/scMINER_dev/test_data/inputs/PBMC_Demo_MICA_input_mini.txt \
-=======
 -i ./test_data/inputs/PBMC_Demo_MICA_input_mini.txt \
->>>>>>> 9b797370bb6cd79b4448be4440731461ef0e043a
 -p "cwl_lsf" \
 -k 3 4 \
 -o ./test_data/outputs/cwl_lsf/ \
--q standard
+-c ./MICA/config/config_cwlexec.json
