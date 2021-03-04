be4ae438b10c4b55440df9bc058b34749ce1fddc
Liang Ding <adamdingliang@gmail.com>
2021-03-04 10:26:07 -0800

    normalize mutual information to distance for clustering

--- tests/test_MICA_f.sh
+++ tests/test_MICA_f.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-./MICA/mica_f.py -i ./test_data/inputs/10x/PBMC/3k/pre-processed/pbmc3k_preprocessed.h5ad -o ./test_data/outputs -j 4 -m mi -d 500
+# ./MICA/mica_f.py -i ./test_data/inputs/10x/PBMC/3k/pre-processed/pbmc3k_preprocessed.h5ad -o ./test_data/outputs -j 4 -m mi -d 500
 
-#./MICA/mica_f.py -i ./test_data/inputs/10x/PBMC/3k/pre-processed/pbmc3k_preprocessed.h5ad -o ./test_data/outputs -j 4 -m euclidean -d 40
+./MICA/mica_f.py -i ./test_data/inputs/10x/PBMC/3k/pre-processed/pbmc3k_preprocessed.h5ad -o ./test_data/outputs -j 4 -m euclidean -r none
+
+# ./MICA/mica_f.py -i /Users/lding/Documents/MICA/Datasets/filtered_gene_bc_matrices/hg19/pbmc33k_preprocessed.h5ad -o /Users/lding/Documents/MICA/Datasets/filtered_gene_bc_matrices/hg19 -j 4 -m euclidean -d 40
+
+# ./MICA/mica_f.py -i /Users/lding/Documents/MICA/Datasets/filtered_gene_bc_matrices/hg19/pbmc33k_preprocessed.h5ad -o /Users/lding/Documents/MICA/Datasets/filtered_gene_bc_matrices/hg19 -j 4 -m mi -d 40
