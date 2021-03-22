f103b8dbe84e25ea3de8a7c6de17b4399e4d304e
Liang Ding <adamdingliang@gmail.com>
2021-03-21 23:52:06 -0700

    add Yan test data

---
+++ tests/test_calc_ARI.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from sklearn.metrics.cluster import adjusted_rand_score
+import pandas as pd
+
+true_label_file = './tests/answerkey/clustering/kolod_true_label_1.txt'
+true_label = pd.read_csv(true_label_file, delimiter='\t', dtype=int, header=0)
+true_label = true_label.iloc[0]
+
+predict_label_file = './test_data/outputs/clustering_umap_euclidean_12.txt'
+predict_label = pd.read_csv(predict_label_file, delimiter='\t')
+predict_label = predict_label['label']
+
+ari = adjusted_rand_score(true_label, predict_label)
+print('Kolod dataset ARI: {}'.format(ari))
+
+
+true_label_file = './tests/answerkey/clustering/yan_true_label_1.txt'
+true_label = pd.read_csv(true_label_file, delimiter='\t', dtype=int, header=0)
+true_label = true_label.iloc[0]
+
+predict_label_file = './test_data/outputs/clustering_umap_euclidean_16.txt'
+predict_label = pd.read_csv(predict_label_file, delimiter='\t')
+predict_label = predict_label['label']
+
+ari = adjusted_rand_score(true_label, predict_label)
+print('Yan dataset ARI: {}'.format(ari))
