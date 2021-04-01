c8054db080deeb14f98240cd9fd219bbba1644a3
Liang Ding <adamdingliang@gmail.com>
2021-04-01 15:07:04 -0500

    minor updates for HPC usage

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "python-louvain>=0.14",
         "fast_histogram>=0.9",
         "node2vec>=0.4.1",
+        "pecanpy>=0.0.1",
         "numba>=0.52.0",
         "anndata>=0.7.4",
         "umap-learn>=0.5.1",
