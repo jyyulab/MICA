47f596e110aa7635819198ca4cb7fd22ed40964a
Liang Ding <adamdingliang@gmail.com>
2021-07-03 00:55:00 -0500

    revoke parameters to reproduce old results

--- MICA/analysis/evaluation/clustering_performance/SilverStd/eval_PBMC_20K.py
+++ MICA/analysis/evaluation/clustering_performance/SilverStd/eval_PBMC_20K.py
@@ -12,4 +12,21 @@ utils.calc_ARIs_mds(root_dir, level, author, num_clusters)
 
 # max_resolution = 10.0
 # utils.create_cmds_ge(root_dir, level, author, input_file, max_resolution)
-utils.calc_ARIs_ge(root_dir, level, author, num_clusters)
+# utils.calc_ARIs_ge(root_dir, level, author, num_clusters)
+
+
+# Check an old commit
+import pandas as pd
+from sklearn.metrics.cluster import adjusted_rand_score
+output_dir = '/research/projects/yu3grp/scRNASeq/yu3grp/LiangDing/MICA/outputs/SilverStd/PBMC_20k/ge_debug'
+
+true_label_file = '/research/rgs01/project_space/yu3grp/scRNASeq/yu3grp/LiangDing/MICA/datasets/' \
+                  'SilverStd/PBMC_20k/PBMC_20k_true_label.txt'
+true_label = pd.read_csv(true_label_file, delimiter='\t', header=0)
+predict_label_file = '{}/clustering_UMAP_euclidean_{}_{}.txt'.format(output_dir, 12, 2.2)
+predict_label = pd.read_csv(predict_label_file, delimiter='\t', index_col=0)
+predict_num_clusters = len(set(predict_label['label']))
+print(predict_num_clusters)
+merged = true_label.merge(predict_label, left_on='cell', right_index=True)
+ari = adjusted_rand_score(merged['label_x'], merged['label_y'])
+print(ari)
