aca794c69f9dc3dbfd9b917913a685bc47fe9c3f
Liang Ding <adamdingliang@gmail.com>
2021-08-24 00:06:31 -0500

    removed Kolod test, added Buettner

--- tests/test_calc_ARI.py
+++ tests/test_calc_ARI.py
@@ -4,13 +4,13 @@ from sklearn.metrics.cluster import adjusted_rand_score
 import pandas as pd
 
 
-true_label_file = './tests/answerkey/clustering/Kolod_true_label.txt'
+true_label_file = './tests/answerkey/clustering/Buettner_true_label.txt'
 true_label = pd.read_csv(true_label_file, delimiter='\t', dtype=int, header=0)
-predict_label_file = './test_data/outputs/Kolod/clustering_UMAP_euclidean_20_4.txt'
+predict_label_file = './test_data/outputs/Buettner/clustering_UMAP_euclidean_20_3.txt'
 predict_label = pd.read_csv(predict_label_file, delimiter='\t', index_col=0)
 merged = true_label.merge(predict_label, left_on='cell', right_index=True)
 ari = adjusted_rand_score(merged['label_x'], merged['label_y'])
-print('Kolod dataset ARI: {}'.format(ari))  # Expected ARI: 0.9302336508092129
+print('Buettner dataset ARI: {}'.format(ari))  # Expected ARI: 0.2662385902792365
 
 
 true_label_file = './tests/answerkey/clustering/Yan_true_label.txt'
